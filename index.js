03274d5df96c441cfd36f2b4fa7920b85c8fa799
primev1bots <niloymina139121@gmail.com>
2025-10-25 12:32:20 +0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 10000;
-
-app.get("/", (_req, res) => res.send("OK"));
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server listening on 0.0.0.0:${PORT}`);
-});
